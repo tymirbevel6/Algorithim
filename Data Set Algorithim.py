a7f590924b45e0ea5b142336602ddb100341c89d
tymirbevel6 <77340060+tymirbevel6@users.noreply.github.com>
2021-05-25 14:44:23 -0400

    Add files via upload

---
+++ Data Set Algorithim.py
@@ -0,0 +1,18 @@
+#Bubblesport
+def bubbleSport(dataset):
+    for i in  range(len(dataset) - 1, 0, -1):
+        for j in range(i):
+            if dataset[j] > dataset[j+1]:
+                temp = dataset[j]
+                dataset[j] = dataset[j+1]
+                dataset[j+1] = temp 
+                
+        print("Current state: ", dataset)
+
+def main():
+    list1 = [6, 20, 8, 19, 56, 23, 87, 41, 49, 53]
+    bubbleSport(list1)
+    print("Result: ", list1)
+
+if __name__ == "__main__":
+    main() 
